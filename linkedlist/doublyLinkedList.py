f38b1a9272a6629116f9a6c983a9f790c8a10dc2
Heroamit007 <amitkumar36273@gmail.com>
2025-10-01 16:02:10 +0530

    doubbly linkedlist

---
+++ linkedlist/doublyLinkedList.py
@@ -0,0 +1,109 @@
+class Node:
+    def __init__(self, prev=None, item=None, next=None):
+        self.prev = prev
+        self.item = item
+        self.next = next
+
+class DLL:
+    def __init__(self, start=None):
+        self.start = start
+    
+    def is_empty(self):
+        return self.start == None
+    def insert_at_start(self,data):
+        n = Node(None, data, self.start )
+        if not self.is_empty():
+            self.start.prev = n
+        self.start = n
+    def insert_at_last(self, data):
+        temp = self.start
+        if self.start != None:
+            while temp.next != None:
+                temp = temp.next
+
+        n = Node(temp, data,None)
+        if temp == None:
+            self.start = n
+        else:
+            temp.next  = n
+
+    def search(self, data):
+        temp = self.start
+        while temp is not None:
+            if temp.item == data:
+                return temp
+            temp = temp.next
+        return None
+    def insert_after(self, temp, data):
+        if temp is not None:
+            n = Node(temp, data, temp.next)
+            if temp.next is not None:
+                temp.next.prev = n
+            temp.next = n
+    def print_List(self):
+        temp = self.start
+        while temp is not None:
+            print(temp.item, end=" ")
+            temp = temp.next
+    def delete_first(self):
+        if self.start is not None:
+            self.start = self.start.next
+            if self.start is not None:
+                self.start.prev = None
+    def delete_last(self):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            self.start = None
+        else:
+            temp = self.start
+            while temp.next is not None:
+                temp = temp.next
+            temp.prev.next = None
+    def delete_item(self, data):
+        if self.start is None:
+            pass
+        else:
+            temp = self.start
+            while temp is not None:
+                if temp.item == data:
+                    if temp.next is not None:
+                        temp.next.prev = temp.prev
+                    if temp.prev is not None:
+                        temp.prev.next = temp.next
+                    else:
+                        self.start = temp.next
+                    break
+                temp = temp.next 
+    def __iter__(self):
+        return DLLIterator(self.start)
+
+class DLLIterator:
+    def __init__(self, start):
+        self.current = start
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.current is None:
+            raise StopIteration
+        else:
+            item = self.current.item
+            self.current = self.current.next
+            return item
+
+# Driver code
+myList = DLL()
+myList.insert_at_start(10)
+myList.insert_at_last(20)
+myList.insert_at_last(30)
+myList.insert_at_start(45)
+myList.print_List()
+print()
+myList.delete_first()
+myList.delete_item(30)
+myList.insert_after(myList.search(20), 25)
+myList.print_List()
+print()
+for i in myList:
+    print(i, end=" ")
+print()
